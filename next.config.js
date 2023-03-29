505cc7ea1bac6163ca6f7a05caf50cc93d248492
TaraOShea <tararotten@gmail.com>
2023-03-29 11:51:38 +0100

    move varibles

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    NEXT_PUBLIC_SANITY_API_VERSION: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+  },
   images: {
     domains: ['cdn.sanity.io'],
   }
--- src/lib/sanity.client.ts
+++ src/lib/sanity.client.ts
@@ -1,5 +1,5 @@
 import {createClient} from 'next-sanity'
-import { dataset, apiVersion } from "./saniry.varibles";
-var projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+import { projectId, dataset, apiVersion } from "./saniry.varibles";
+
 
 export const client = createClient({projectId, dataset, apiVersion, useCdn: true})
