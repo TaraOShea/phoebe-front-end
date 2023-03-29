a8b0402d2aaea1092aacea23117f5f89303793cc
TaraOShea <tararotten@gmail.com>
2023-03-29 14:01:44 +0100

    remove post

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,18 +4,17 @@ import Link from "next/link";
 import { SanityDocument } from "@sanity/client";
 
 function Header({ categories }: { categories: SanityDocument[] }) {
-    console.log(categories)
  return (
    <header className="header">
         <div>Main Feed</div>
         <ul>
-        {/* {categories.map((category) => (  
+        {categories.map((category) => (  
             <li key={category._id}>
                  <Link href={category.slug.current} >
                     {category.name}
                 </Link>
             </li>
-        ))} */}
+        ))}
         </ul>
         <button>Information</button>
    </header>
