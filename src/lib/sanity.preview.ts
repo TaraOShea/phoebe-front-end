eb02e3727fd69fc5c4124231779a65bfa9f45be3
TaraOShea <tararotten@gmail.com>
2023-03-29 12:09:10 +0100

    move varibles

--- src/lib/sanity.preview.ts
+++ src/lib/sanity.preview.ts
@@ -1,7 +1,7 @@
 import {definePreview} from 'next-sanity/preview'
-import { dataset } from "./sanity.varibles";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 
 function onPublicAccessOnly() {
   throw new Error(`Unable to load preview as you're not logged in`)
