8734414c93fb000ddfd04734d279c4e64c7f6410
saschakrischock <s.krischock@gmail.com>
2024-03-15 18:06:25 +0100

    changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,7 +37,7 @@ export const getServerSideProps = async ({ preview = false }) => {
   
   var data = await client.fetch(groq`
   {
-    'posts': *[_type == "post" &&  'runway' in post_category[].category_list->slug.current] | order(publishDate desc) {
+    'posts': *[_type == "post" &&  'runway' in post_category[].category_list->slug.current] | order(publishDate asc) {
       _id,
       title, 
       slug,
